7570a783499efb1070533d7e6556a2c21edb6b46
Diana Shimlykh <d56671250@gmail.com>
2026-03-26 01:25:00 +0300

    feat: two modals+ helper animate

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,9 @@ import "../../css/style.css";
 import { phoneArrow } from "../modules/phoneArrow";
 import { menu } from "../modules/menu";
 import { smoothScroll } from "../modules/smoothScroll";
+import { modal } from "../modules/modal";
 
 phoneArrow();
 menu();
 smoothScroll();
+modal();
---
+++ src/modules/modal.js
@@ -0,0 +1,62 @@
+import { animate } from "./helpers";
+
+export const modal = () => {
+  const popup = document.querySelector(".popup-repair-types");
+  const menuBody = document.querySelector(".popup-dialog-menu");
+  const menuBtn = document.querySelector(".menu__icon");
+
+  if (!popup) return;
+
+  const appear = () => {
+    if (menuBody && menuBody.classList.contains('__active')) {
+      menuBody.classList.remove('__active');
+      menuBtn?.classList.remove('__active');
+    }
+
+    popup.style.display = "block";
+    popup.style.visibility = "visible";
+    popup.style.opacity = 0;
+
+    if (window.innerWidth < 768) {
+      popup.style.opacity = 1;
+    } else {
+      animate({
+        duration: 300,
+        timing: (t) => t,
+        draw(progress) {
+          popup.style.opacity = progress;
+        },
+      });
+    }
+  };
+
+  const hide = () => {
+    if (window.innerWidth < 768) {
+      popup.style.display = "none";
+      popup.style.visibility = "hidden";
+    } else {
+      animate({
+        duration: 300,
+        timing: (t) => t,
+        draw(progress) {
+          popup.style.opacity = 1 - progress;
+          if (progress === 1) {
+            popup.style.display = "none";
+            popup.style.visibility = "hidden";
+          }
+        },
+      });
+    }
+  };
+
+  document.addEventListener("click", (e) => {
+    const openBtn = e.target.closest(".link-list-menu, .link-list-repair");
+    if (openBtn) {
+      e.preventDefault();
+      appear();
+    }
+    if (e.target.closest(".close") || (e.target === popup)) {
+       hide();
+    }
+  });
+};
